b030f6c78a1f5da75e44f2c4043e5c8733cd6b3f
Gabor Kiss <gaborkiss02@gmail.com>
2023-08-03 13:10:52 +0200

    Game logic finish, winner animation added

--- src/Components/Fields/Field.js
+++ src/Components/Fields/Field.js
@@ -9,6 +9,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faX } from '@fortawesome/free-solid-svg-icons'
 import { faCircle } from '@fortawesome/free-regular-svg-icons'
 
+
+function winnerDecider (arr) {
+    if ((arr.includes(0) && arr.includes(1) && arr.includes(2))) return [0, 1 ,2];
+    if ((arr.includes(3) && arr.includes(4) && arr.includes(5))) return [3, 4 ,5];
+    if ((arr.includes(6) && arr.includes(7) && arr.includes(8))) return [6, 7 ,8];
+    if ((arr.includes(0) && arr.includes(3) && arr.includes(6))) return [0, 3 ,6];
+    if ((arr.includes(1) && arr.includes(4) && arr.includes(7))) return [1, 4 ,7];
+    if ((arr.includes(2) && arr.includes(5) && arr.includes(8))) return [2, 5 ,8];
+    if ((arr.includes(2) && arr.includes(4) && arr.includes(6))) return [2, 4 ,6];
+    if ((arr.includes(0) && arr.includes(4) && arr.includes(8))) return [0, 4 ,8];
+}
+
 function Field (props) {
     const [activePlayer, setActivePlayer] = useContext(ActivePlayer)
     const [buttonClicked, setButtonClicked] = useState(false);
@@ -16,6 +28,8 @@ function Field (props) {
 
     const player1Arr = useContext(Player1Arr);
     const player2Arr = useContext(Player2Arr);
+
+    const activePlayerArr = !activePlayer ? player1Arr : player2Arr;
 
     function clickHandle() {
         props.playerHandler()
@@ -26,8 +40,11 @@ function Field (props) {
     if (buttonClicked && playerClicked && !player1Arr.includes(props.index)) player1Arr.push(props.index)
     if (buttonClicked && !playerClicked && !player2Arr.includes(props.index)) player2Arr.push(props.index)
 
+    const winnerArr = winnerDecider(activePlayerArr);
+    props.winnerArrFunc(winnerArr)
+
     return (
-        <div className={styles.container} onClick={!buttonClicked ? clickHandle : undefined}>
+        <div className={`${styles.container} ${props.winnerElements && styles.win} ${!winnerArr && styles.default}`} onClick={!buttonClicked && winnerArr == undefined ? clickHandle : undefined}>
             {buttonClicked && playerClicked && <FontAwesomeIcon icon={faX} size={'10x'} style={{position: 'absolute'}} color='#61c0bc'/>}
             {buttonClicked && !playerClicked && <FontAwesomeIcon icon={faCircle} size={'10x'} style={{position: 'absolute'}} color='#e5ac4d'/>}
         </div>
--- src/Components/Fields/FieldContainer.js
+++ src/Components/Fields/FieldContainer.js
@@ -1,25 +1,48 @@
 import styles from './FieldContainer.module.css'
-
 import Field from './Field'
 import { ActivePlayer } from '../../Context/Context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 function FieldContainer () {
     const arr = [0,1,2,3,4,5,6,7,8];
     
     const [activePlayer, setActivePlayer] = useContext(ActivePlayer)
 
+    const [winnerArr, setWinnerArr] = useState();
+
     function playerHandler() {
         setActivePlayer(!activePlayer);
     }
 
-    console.log(activePlayer);
+    function winnerArrFunc(arr) {
+        if (!arraysEqual(arr, winnerArr)) {
+            setWinnerArr(arr);
+        }
+    }
+
+    function arraysEqual(arr1, arr2) {
+        if (arr1 === arr2) return true;
+        if (arr1 == null || arr2 == null) return false;
+        if (arr1.length !== arr2.length) return false;
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) return false;
+        }
+        return true;
+    }
+
+    console.log(winnerArr);
 
     return (
-        <section className={styles['field-container']}>
-        {arr.map((item, index) =>
-            <Field key={index} index={index} playerHandler={playerHandler}></Field>
-        )}
+        <section className={styles.container}>
+            {arr.map((item, index) =>
+                <Field 
+                    key={item} 
+                    index={item} 
+                    playerHandler={playerHandler} 
+                    winnerArrFunc={winnerArrFunc} 
+                    winnerElements={winnerArr && winnerArr.includes(index)}>
+                </Field>
+            )}
         </section>
     )
 }
